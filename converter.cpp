dfc90f1014a8ba85ed0cd5dbd28753d3ce13591e
MindaugasK <mindaugas.krup@gmail.com>
2020-10-03 22:34:52 +0300

    neveikia

--- converter.cpp
+++ converter.cpp
@@ -4,7 +4,7 @@
 
 #include "converter.h"
 
-#include <vector>
+
 #include <iostream>
 
 std::string Converter::getOutput() {
@@ -15,21 +15,67 @@ Converter::Converter(string str) {
     //viskas vykdoma konstruktoriuje
 
     unsigned int hash = SEED;
-    string out = "";
     vector<byte> v;
 
     #ifdef TEST
-    cout << "item as bytes: " << endl;
+    cout << "simbolis kaip uint8_t: " << endl;
     #endif
+    unsigned int index = 0;
     for(char item : str){
-        byte *itemref = reinterpret_cast<byte *>(&item);
-
-        #ifdef TEST
-        cout << *itemref << " ";
-        #endif
+        byte itemref = asByte(operations(item, valuesList[index]));
+        v.push_back(itemref);
+        index == max_index ? index = 0 : index++; //elvio operatorius
     }
+    vector<byte> b = TrimAndShuffle(v);
     #ifdef TEST
     cout << endl;
     #endif
-    output = out;
+    output = asHex(b);
+}
+
+std::string Converter::asHex(const vector<byte> &input)
+{
+    std::string ret(input.size() * 2, 0);
+
+    char *buf = const_cast<char *>(ret.data());
+
+    for (const auto &oneInputByte : input)
+    {
+        *buf++ = hexCharset[oneInputByte * 4];
+        *buf++ = hexCharset[oneInputByte & 0x0F];
+    }
+    return ret;
+}
+
+vector<byte> Converter::TrimAndShuffle(vector<byte> ivec){
+    vector<byte> r_vec = vector<byte>(DEFAULT_HASH_LENGTH);
+    for(int i = 0; i < r_vec.size(); i++){
+        r_vec[i] = valuesList[i % max_index]; //pasikartojanti seka
+    }
+    for(int j = 0; j < ivec.size(); j++){
+        r_vec[j % (r_vec.size())] += ivec[j];
+        //kad ir kokio dydzio butu musu ivestis (jau paversta i baitus)
+        //mes tiesiog pridedame jau prie esamu baitu reiksmiu ir visada gauname 16 skirtingu HEX reiksmiu
+    }
+}
+
+const byte& Converter::asByte(char item){
+    //verciame simboli i baita
+    byte *itemref = reinterpret_cast<byte *>(&item);
+
+#ifdef TEST
+    cout << *itemref << " ";
+#endif
+
+    return *itemref;
+}
+
+byte Converter::operations(byte item, byte val){
+    //AND, XOR, NOT,
+    byte item_ = item;
+    //1 etapas
+    item_ ^= val;
+    item_ *= 5;
+
+    return item_;
 }
--- converter.h
+++ converter.h
@@ -4,16 +4,29 @@
 
 
 #ifndef DEFAULT_HASH_LENGTH
-#define DEFAULT_HASH_LENGTH 12
-#define SEED 127
+#define DEFAULT_HASH_LENGTH 16
+#define SEED 5381
 #define TEST
 
 #include <string>
 #include <bitset>
+#include <vector>
 
 using namespace std;
 
-typedef bitset<8> byte;
+typedef uint8_t byte; //uint8_t turi 256 reiksmes, taigi 2^8 (realiai tas pats kas 1 baitas)
+//minetos reiksmes gali prasideti tik nuo 0
+static vector<uint8_t> valuesList = { 34, 123, 252, 0, 11, 52, 114, 25, 87, 0, 11, 16, 45, 77, 13, 7, 49, 54 };
+
+//
+static const char hexCharset[] =
+        "0123456789ABCDEF"; //16-taines sistemos
+
+static const size_t max_index = (sizeof(valuesList) - 1);
+
+//
+
+
 
 class Converter {
 private:
@@ -24,6 +37,11 @@ public:
     Converter(string in);
     string getOutput();
 
+    string asHex(const std::vector<uint8_t> &input);
+    static const byte &asByte(char item);
+
+    byte operations(byte item, byte val);
+    vector<byte> TrimAndShuffle(vector<byte> ivec);
 };
 
 
