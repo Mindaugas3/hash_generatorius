3ae30391eb35ba54546a39a42cc0e9c8b1462972
MindaugasK <mindaugas.krup@gmail.com>
2020-10-09 00:29:23 +0300

    argc, argv
    Programa jau gali paimti argumentus komandineje eiluteje.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char **argv) {
 
 
-    if(argc > 0){
-        IO io = IO((string) *argv);
+    if(argc > 1){
+        IO io = IO((string) argv[1]);
         Converter converter = Converter(io.getInput());
         io.Output(converter.getOutput());
     } else {
