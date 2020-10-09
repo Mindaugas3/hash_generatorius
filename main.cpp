1078b116c0630abee03cfb002a4cd76a17b249b2
MindaugasK <mindaugas.krup@gmail.com>
2020-10-09 03:01:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "IO.h"
 #include "converter.h"
 #include "Tests.h"
@@ -6,10 +7,13 @@
 int main(int argc, char **argv) {
 
 
-    if(argc == 2){ //nurodytas vienas failas
+    if(argc == 2){ //nurodytas vienas failas - matuojamas laikas
         IO io = IO((string) argv[1]);
+        auto start = std::chrono::system_clock::now();
         Converter converter = Converter(io.getInput());
-        IO::Output(converter.getOutput());
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> diff = end-start;
+        std::cout << "Laikas per kuri buvo suhasuotas failas: "<< argv[1] << " : " << diff.count() << " s\n";
     } else if(argc > 2){ //nurodyti keli failai
         IO io = IO(argv[1]);
         IO io2 = IO(argv[2]);
