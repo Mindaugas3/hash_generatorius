fd6becc3ad170219e69b6a843deba0bd0b21f9e5
MindaugasK <mindaugas.krup@gmail.com>
2020-10-11 20:52:33 +0300

    istryniau komentarus

--- converter.h
+++ converter.h
@@ -13,18 +13,13 @@
 
 using namespace std;
 
-typedef uint8_t byte; //uint8_t turi 256 reiksmes, taigi 2^8 (realiai tas pats kas 1 baitas)
-//minetos reiksmes gali prasideti tik nuo 0
+typedef uint8_t byte;
 static vector<uint8_t> valuesList = { 34, 123, 252, 0, 11, 52, 114, 25, 87, 0, 11, 16, 45, 77, 13, 7, 49, 54 };
 
-//
-static const char hexCharset[] =
-        "0123456789ABCDEF"; //16-taines sistemos
+
+static const char hexCharset[] = "0123456789ABCDEF"; //16-taines sistemos
 
 static const size_t max_index = valuesList.size() - 1;
-
-//
-
 
 
 class Converter {
@@ -39,7 +34,6 @@ public:
     vector<byte> getOutput();
 
     static byte operations(byte item, byte val);
-
     static vector<byte> TrimAndFill(vector<byte> ivec);
     static unsigned long long int addAll(vector<byte> items);
 
